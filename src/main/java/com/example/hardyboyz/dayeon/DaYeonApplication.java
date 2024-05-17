2e8f559374b8d9dc65513a0e617582a0a7738aed
Meringue-KIm <dd8873@naver.com>
2024-05-17 22:10:46 +0900

    t

---
+++ src/main/java/com/example/hardyboyz/dayeon/DaYeonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hardyboyz.dayeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaYeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaYeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hardyboyz/dayeon/DaYeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hardyboyz.dayeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaYeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
